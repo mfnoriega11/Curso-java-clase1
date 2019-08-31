03402c66dffe460abe813ba05645277ead5b5e48
s109e1 <manuelillo92@gmail.com>
2019-08-31 09:40:06 -0500

    segundo commit

--- src/ejercicionro1/EjercicioNro1.java
+++ src/ejercicionro1/EjercicioNro1.java
@@ -16,6 +16,14 @@ public class EjercicioNro1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int num2; 
+        int num1;
+        int sum;
+       num2=2;
+       num1=2;
+       sum=num1+num2;
+        
+        
     }
     
 }
---
+++ src/ejercicionro1/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionro1;
+
+/**
+ *
+ * @author s109e1
+ */
+public class NewClass {
+   int num1;
+   
+   
+
+   
+   
+}
