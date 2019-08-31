98af3101fae0aa8ccb2473e601282d9f52de58a1
s109e1 <manuelillo92@gmail.com>
2019-08-31 09:01:18 -0500

    primer commit

---
+++ src/ejercicionro1/EjercicioNro1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionro1;
+
+/**
+ *
+ * @author s109e1
+ */
+public class EjercicioNro1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
